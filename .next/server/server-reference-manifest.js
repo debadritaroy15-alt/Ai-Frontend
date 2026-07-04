967547b2f363f9207802be6c3dcd422ece9dc943
DEBADRITA PROJECT <debaritaroy15@gmail.com>
2026-07-04 10:27:18 +0530

    delete .next

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"eG0H7/zQk32s0NAsTS16DG5uQyn15UP/JsvnprDUcsY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- .next/static/chunks/src_app_auth_page_tsx_8f3504f5._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_3f42efbf._.js",
-  "static/chunks/src_e4db320a._.js"
-],
-    source: "dynamic"
-});
